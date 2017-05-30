f1287c7dc412edf78fe1676ef6d4370fd6d0907b
Sam <samantha.mar017@gmail.com>
2017-05-30 15:03:29 +0000

    Finish layout and routes

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+     assert_select "a[href=?]", signup_path 
+  end
+  
+  test "signup title" do 
+    get signup_path
+     assert_equal full_title,         "Ruby on Rails Tutorial Sample App" 
+    assert_equal full_title("Sign up"), "Sign up | Ruby on Rails Tutorial Sample App"
+    
+  end
+end
