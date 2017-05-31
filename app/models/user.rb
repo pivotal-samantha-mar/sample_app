cc7a3a0378f38b672027f213214f0b4f576926e9
Sam <samantha.mar017@gmail.com>
2017-05-31 21:11:15 +0000

    Make a basic user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :name, :email
+    #attr_accessor :name, :email
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
